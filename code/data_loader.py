f425887077267e7c87255b9b418c4bee2504e91c
eday1632 <eric.day87@gmail.com>
2018-06-21 13:41:51 -0700

    added try except to prevent one file from messing up loading of the others

--- code/data_loader.py
+++ code/data_loader.py
@@ -19,26 +19,33 @@ logger = logging.getLogger('data_loader')
 
 
 class DataLoader(object):
+    """
+    """
     def __init__(self, datapath):
         self.datapath = datapath
 
     def load(self):
+        """
+        """
         with open('/etc/calfresh/logs/calfresh.log', 'a') as logfile:
             for root, dirs, files in walk(self.datapath):
                 for table_name in files:
                     logger.info('Loading %s', table_name)
                     with open(join(self.datapath, table_name)) as csvfile:
                         header = csv.reader(csvfile, delimiter=',').next()
-                        result = subprocess.call([
-                            'mysqlimport',
-                            '--local',
-                            '--replace',
-                            '--fields-terminated-by=,',
-                            '--ignore-lines=1',
-                            '--columns=' + ','.join(header),  # dynamic
-                            '-u',
-                            'eday',
-                            'calfreshdb',
-                            join(self.datapath, table_name),  # dynamic
-                        ], stdout=logfile)
-                        logger.info('Load result: %s', result)
+                        try:
+                            result = subprocess.call([
+                                'mysqlimport',
+                                '--local',
+                                '--replace',
+                                '--fields-terminated-by=,',
+                                '--ignore-lines=1',
+                                '--columns=' + ','.join(header),
+                                '-u',
+                                'eday',
+                                'calfreshdb',
+                                join(self.datapath, table_name),
+                            ], stdout=logfile)
+                            logger.info('Load result: %s', result)
+                        except Exception as ex:
+                            logger.exception(ex)

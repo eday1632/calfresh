f50c64396fbbb3c150cfd5047db28457854f9e80
eday1632 <eric.day87@gmail.com>
2018-06-22 15:09:30 -0700

    pandas versions, sheesh

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'editdistance(==0.1.4)',
         'numpy(>=1.7.0)',
         'openpyxl(>=2.4.7)',
-        'pandas(==0.21.0)',
+        'pandas(==0.20.1)',
         'requests(>=2.14.2)',
         'xlrd(>=1.0.0)',
     ],

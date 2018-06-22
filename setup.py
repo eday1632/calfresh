d5be77440868f63b84566889b51f73b1ed0d57a0
eday1632 <eric.day87@gmail.com>
2018-06-22 12:43:18 -0700

    ran the daily check. updating files.

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name='CalFresh',
     version='1.0',
+    description='CalFresh ETL System',
     author='Eric Day',
     author_email='ericday87@gmail.com',
     url='www.calfreshdb.org',
     packages=['calfresh'],
     license='MIT License',
     long_description=open('README.txt').read(),
-    install_requires=[
-        'BeautifulSoup4 >= 4.6.0',
-        'editdistance == 0.1.4',
-        'numpy >= 1.7.0',
-        'openpyxl >= 2.4.7',
-        'pandas >= 0.20.1',
-        'requests >= 2.14.2',
-        'xlrd >= 1.0.0',
+    requires=[
+        'BeautifulSoup4(>=4.6.0)',
+        'editdistance(==0.1.4)',
+        'numpy(>=1.7.0)',
+        'openpyxl(>=2.4.7)',
+        'pandas(>=0.20.1)',
+        'requests(>=2.14.2)',
+        'xlrd(>=1.0.0)',
     ],
 )
